1d5977723b18ecd71295d91aef5e4286afd1b41e
amgillenwater <amgillenwater@gmail.com>
2020-06-04 13:53:40 -0400

    .

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+class Habit(models.model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='habits')
+    verb = models.CharField(max_length=260)
+    goal_quant = models.PositiveIntegerField(null=True,blank=True)
+    
+    # def __str__(self):
+
+class DailyRecord(models.model):
+    habit = models.ForeignKey(to=Habit, on_delete=models.CASCADE, related_name='records')
+    recorded_on = models.DateField(auto_now=True)
+
+
