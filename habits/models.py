7d00b79485f3e9a85a818329efc97416795b12c2
amgillenwater <amgillenwater@gmail.com>
2020-06-04 21:54:34 -0400

    about to start on views

--- habits/models.py
+++ habits/models.py
@@ -3,13 +3,18 @@ from users.models import User
 # Create your models here.
 class Habit(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='habits')
-    verb = models.CharField(max_length=260)
-    goal_quant = models.PositiveIntegerField(null=True,blank=True)
-    
-    # def __str__(self):
+    goal = models.CharField(max_length=260)
+    goal_quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.goal
+
 
 class DailyRecord(models.Model):
     habit = models.ForeignKey(to=Habit, on_delete=models.CASCADE, related_name='records')
+    quantity = models.PositiveIntegerField()
     recorded_on = models.DateField(auto_now=True)
-
+    
+    class Meta():
+        unique_together=[['habit', 'recorded_on']]
 
