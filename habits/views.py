5167190563737c445276e0229b6faf16501f9ac8
amgillenwater <amgillenwater@gmail.com>
2020-06-06 16:50:52 -0400

    authenticated user decorator added to views.py

--- habits/views.py
+++ habits/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
+from users.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def homepage(request):
     if request.user.is_authenticated:
         return redirect(to='habit_list')
+    
     return render(request,"habits/home.html")
 
+@login_required
 def habit_list(request):
     habits = request.user.habits.all()
-    return render(request, "habits/habit_list.html", {'habits': habits}) 
+    return render(request, "habits/habit_list.html", {"habits": habits}) 
+
