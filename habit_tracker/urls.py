11c9d675725f9650e64717f1344f8130c3472c0c
amgillenwater <amgillenwater@gmail.com>
2020-06-06 14:08:10 -0400

    homepage work--login authentic

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -19,7 +19,8 @@ from django.urls import include, path
 from habits import views as habits_views
 
 urlpatterns = [
-    path('', habits_views.homepage), 
+    path('', habits_views.homepage, name="homepage"), 
+    path('habits/', habits_views.habit_list, name="habit_list"),
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def homepage(request):
+    if request.user.is_authenticated:
+        return redirect(to='habit_list')
     return render(request,"habits/home.html")
+
+def habit_list(request):
+    habits = request.user.habits.all()
+    return render(request, "habits/habit_list.html", {'habits': habits}) 
